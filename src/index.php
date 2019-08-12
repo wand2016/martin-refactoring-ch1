42b4ce9c62d786a85a2829ebca58d36278f0c353
wand2016 <d19921207@gmail.com>
2019-08-12 20:20:05 +0900

    renderHTML関数の追加
    - 本筋からそれるのでstub
    - 中間データ生成を分離したため、コピペせずにすんだ
    - ほかのrender関数から利用するために`usd()`関数を外だし

--- src/index.php
+++ src/index.php
@@ -2,28 +2,33 @@
 
 use App\CreateStatementData;
 
-function statement($invoice, $plays){
+function statement($invoice, $plays)
+{
     $createStatementData = new CreateStatementData();
     return renderPlainText($createStatementData($invoice, $plays));
 }
 
 function renderPlainText($data)
 {
-    $usd = function ($aNumber) {
-        $format = '$%.2f';
-        return sprintf($format, $aNumber / 100);
-    };
-
-    // ----------------------------------------
-
-
     $result = "Statement for ${data['customer']}";
     foreach ($data['performances'] as $perf) {
         // print line for this order
-        $result .= '  ' . $perf['play']['name'] . ': ' . $usd($perf['amount']) . "(${perf['audience']} seats)" . PHP_EOL;
+        $result .= '  ' . $perf['play']['name'] . ': ' . usd($perf['amount']) . "(${perf['audience']} seats)" . PHP_EOL;
     }
 
-    $result .= 'Amount owed is ' . $usd($data['totalAmount']) . PHP_EOL;
+    $result .= 'Amount owed is ' . usd($data['totalAmount']) . PHP_EOL;
     $result .= 'You earned ' . $data['totalVolumeCredits'] . ' credits' . PHP_EOL;
     return $result;
+}
+
+// stub
+function renderHtml($data)
+{
+    return '';
+}
+
+function usd($aNumber)
+{
+    $format = '$%.2f';
+    return sprintf($format, $aNumber / 100);
 }
