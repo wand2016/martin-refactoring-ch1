0a6ada6f269d8d2c151e50feac5aee645ffa1882
wand2016 <d19921207@gmail.com>
2019-08-12 20:41:53 +0900

    amount計算ロジックをPerformanceCalculatorクラスに移動

--- src/CreateStatementData.php
+++ src/CreateStatementData.php
@@ -8,31 +8,17 @@ class CreateStatementData
 {
     public function __invoke($invoice, $plays)
     {
-        $amountFor = function ($aPerformance) {
-            $result = 0;
-            switch ($aPerformance['play']['type']) {
-                case 'tragedy':
-                    $result = 40000;
-                    if ($aPerformance['audience'] > 30) {
-                        $result += 1000 * ($aPerformance['audience'] - 30);
-                    }
-                    break;
-                case 'comedy':
-                    $result = 30000;
-                    if ($aPerformance['audience'] > 20) {
-                        $result += 10000 + 500 * ($aPerformance['audience'] - 20);
-                    }
-                    $result += 300 * $aPerformance['audience'];
-                    break;
-                default:
-                    throw new Error('unknown type: ' . $aPerformance['play']['type']);
-            }
-
-            return $result;
-        };
-
         $playFor = function ($perf) use ($plays) {
             return $plays[$perf['playID']];
+        };
+
+        $amountFor = function ($aPerformance) use (
+            $playFor
+        ){
+            return (new PerformanceCalculator(
+                $aPerformance,
+                $playFor($aPerformance)
+            ))->amount();
         };
 
         $volumeCreditsFor = function ($aPerformance) {
--- src/PerformanceCalculator.php
+++ src/PerformanceCalculator.php
@@ -20,4 +20,27 @@ class PerformanceCalculator
     {
         return $this->aPlay;
     }
+
+    public function amount()
+    {
+        $result = 0;
+        switch ($this->play()['type']) {
+            case 'tragedy':
+                $result = 40000;
+                if ($this->aPerformance['audience'] > 30) {
+                    $result += 1000 * ($this->aPerformance['audience'] - 30);
+                }
+                break;
+            case 'comedy':
+                $result = 30000;
+                if ($this->aPerformance['audience'] > 20) {
+                    $result += 10000 + 500 * ($this->aPerformance['audience'] - 20);
+                }
+                $result += 300 * $this->aPerformance['audience'];
+                break;
+            default:
+                throw new Error('unknown type: ' . $this->play()['type']);
+        }
+        return $result;
+    }
 }
