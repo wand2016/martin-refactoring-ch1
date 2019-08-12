d7830f04ba6c2f9b469363d85a42fa8abcc7d5f2
wand2016 <d19921207@gmail.com>
2019-08-12 18:24:20 +0900

    Decomposing the `statement` function
    pp.1-23

--- src/index.php
+++ src/index.php
@@ -2,44 +2,76 @@
 
 function statement($invoice, $plays)
 {
-    $totalAmount = 0;
-    $volumeCredits = 0;
-    $result = "Statement for ${invoice['customer']}";
-    $format = '$%.2f';
+    $playFor = function ($perf) use ($plays) {
+        return $plays[$perf['playID']];
+    };
 
-    foreach ($invoice['performances'] as $perf) {
-        $play = $plays[$perf['playID']];
-        $thisAmount = 0;
-
-        switch ($play['type']) {
+    $amountFor = function ($aPerformance) use ($playFor) {
+        $result = 0;
+        switch ($playFor($aPerformance)['type']) {
             case 'tragedy':
-                $thisAmount = 40000;
-                if ($perf['audience'] > 30) {
-                    $thisAmount += 1000 * ($perf['audience'] - 30);
+                $result = 40000;
+                if ($aPerformance['audience'] > 30) {
+                    $result += 1000 * ($aPerformance['audience'] - 30);
                 }
                 break;
             case 'comedy':
-                $thisAmount = 30000;
-                if ($perf['audience'] > 20) {
-                    $thisAmount += 10000 + 500 * ($perf['audience'] - 20);
+                $result = 30000;
+                if ($aPerformance['audience'] > 20) {
+                    $result += 10000 + 500 * ($aPerformance['audience'] - 20);
                 }
-                $thisAmount += 300 * $perf['audience'];
+                $result += 300 * $aPerformance['audience'];
                 break;
             default:
-                throw new Error("unknown type: ${$play['type']}");
+                throw new Error('unknown type: ' . $playFor($aPerformance)['type']);
         }
 
-        // add volume credits
-        $volumeCredits += max($perf['audience'] - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ('comedy' === $play['type']) $volumeCredits += floor($perf['audience'] / 5);
+        return $result;
+    };
 
+    $volumeCreditsFor = function ($aPerformance) use ($playFor) {
+        $result = 0;
+        $result += max($aPerformance['audience'] - 30, 0);
+        if ('comedy' === $playFor($aPerformance)['type']) $result += floor($aPerformance['audience'] / 5);
+        return $result;
+    };
+
+    $usd = function ($aNumber) {
+        $format = '$%.2f';
+        return sprintf($format, $aNumber / 100);
+    };
+
+    $totalVolumeCredits = function () use (
+        $invoice,
+        $volumeCreditsFor
+    ) {
+        $volumeCredits = 0;
+        foreach ($invoice['performances'] as $perf) {
+            $volumeCredits += $volumeCreditsFor($perf);
+        }
+        return $volumeCredits;
+    };
+
+    $totalAmount = function () use ($invoice, $amountFor) {
+        $result = 0;
+        foreach ($invoice['performances'] as $perf) {
+            $result += $amountFor($perf);
+        }
+        return $result;
+    };
+
+
+
+    // ----------------------------------------
+
+
+    $result = "Statement for ${invoice['customer']}";
+    foreach ($invoice['performances'] as $perf) {
         // print line for this order
-        $result .= "  ${play['name']}: " . sprintf($format, $thisAmount / 100) . "(${perf['audience']} seats)" . PHP_EOL;
-        $totalAmount += $thisAmount;
+        $result .= '  ' . $playFor($perf)['name'] . ': ' . $usd($amountFor($perf)) . "(${perf['audience']} seats)" . PHP_EOL;
     }
 
-    $result .= 'Amount owed is ' . sprintf($format, $totalAmount / 100) . PHP_EOL;
-    $result .= "You earned ${volumeCredits} credits" . PHP_EOL;
+    $result .= 'Amount owed is ' . $usd($totalAmount()) . PHP_EOL;
+    $result .= 'You earned ' . $totalVolumeCredits() . ' credits' . PHP_EOL;
     return $result;
 }
